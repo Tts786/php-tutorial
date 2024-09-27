6766c2e68b7efdd54c1929a93013886b60b6ef14
Tts786 <sattednasir722@gmali.com>
2024-09-27 17:08:54 +0530

    updated

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Hello php-tutorial</h1>
+    <?php 
+   /* $color = "red";
+    echo "My car is " . $color . "<br>";
+    echo "My house is " . $color . "<br>";
+    echo "My boat is " . $color . "<br>";*/
+
+//Creating (Declaring) PHP Variables
+$x = 5;
+$y = "John";
+
+echo $x;
+echo "<br>";
+echo $y;
+echo"<br>";
+echo 10+10
+
+    ?>
+</body>
+</html>
